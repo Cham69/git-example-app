4014a3ae60387639246a5235008e7f9e5c3420c2
Chamaka <jayashanchamaka@gmail.com>
2025-02-27 22:44:22 +0530

    Added a console log from console branch

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 console.log("Hello World")
 console.log("Another line added")
 console.log("Added this from dev branch")
-console.log("Meew feature added by Chamaka feature branch")
+console.log("Meew feature added by console-branch")
